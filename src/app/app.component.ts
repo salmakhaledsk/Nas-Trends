0563fe9706a7b810c82671d61701bab9082fb180
Salma Khaled <salma283990@gmail.com>
2025-10-27 06:00:35 +0300

    handleError

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+// import { NavbarComponent } from './components/navbar/navbar.component';
+// import { FooterComponent } from './components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,FooterComponent,RouterOutlet],
+  // imports: [NavbarComponent,FooterComponent,RouterOutlet],
+ imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,18 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server // or RenderMode.Client if you prefer pure client rendering
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
